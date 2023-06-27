548a41bdaf9d6d3c0cd1785060891e280514a985
rewritablehere <ananthacp@gmail.com>
2023-06-27 18:26:44 -0400

    Update DataViz.js

--- DataViz.js
+++ DataViz.js
@@ -1,39 +1,62 @@
-let years = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020];
-let population = [100, 120, 150, 180, 200, 220, 240, 260, 280, 300, 320];
-let graduates = [10, 12, 15, 18, 20, 22, 24, 26, 28, 30, 32];
+let cubeSize = 100;
+let angleX = 0;
+let angleY = 0;
+let startX, startY;
+let isDragging = false;
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
 function draw() {
   background(220);
 
-  // Draw x-axis
-  stroke(0);
-  line(50, height - 50, width - 50, height - 50);
+  if (isDragging) {
+    let dx = mouseX - startX;
+    let dy = mouseY - startY;
 
-  // Draw y-axis for population
-  stroke(255, 0, 0);
-  line(50, height - 50, 50, 50);
+    angleX += dx * 0.01;
+    angleY += dy * 0.01;
 
-  // Draw y-axis for graduates
-  stroke(0, 0, 255);
-  line(width - 50, height - 50, width - 50, 50);
-
-  // Plot population data
-  stroke(255, 0, 0);
-  for (let i = 0; i < years.length; i++) {
-    let x = map(years[i], years[0], years[years.length - 1], 50, width - 50);
-    let y = map(population[i], min(population), max(population), height - 50, 50);
-    point(x, y);
+    startX = mouseX;
+    startY = mouseY;
   }
 
-  // Plot graduates data
+  rotateX(angleY);
+  rotateY(angleX);
+
+  // Draw axes
+  drawAxes();
+
+  // Draw the cube
+  box(cubeSize);
+}
+
+function drawAxes() {
+  // X-axis (red)
+  stroke(255, 0, 0);
+  line(0, 0, 0, 100, 0, 0);
+
+  // Y-axis (green)
+  stroke(0, 255, 0);
+  line(0, 0, 0, 0, 100, 0);
+
+  // Z-axis (blue)
   stroke(0, 0, 255);
-  for (let i = 0; i < years.length; i++) {
-    let x = map(years[i], years[0], years[years.length - 1], 50, width - 50);
-    let y = map(graduates[i], min(graduates), max(graduates), height - 50, 50);
-    point(x, y);
-  }
+  line(0, 0, 0, 0, 0, 100);
+}
+
+function mousePressed() {
+  startX = mouseX;
+  startY = mouseY;
+  isDragging = true;
+}
+
+function mouseReleased() {
+  isDragging = false;
+}
+
+function windowResized() {
+  // Resize the canvas when the window is resized
+  resizeCanvas(windowWidth, windowHeight, WEBGL);
 }
